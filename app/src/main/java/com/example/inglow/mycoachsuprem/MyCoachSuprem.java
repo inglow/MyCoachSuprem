263362ffbaf483ce467d43374d115f66f703d1f8
inglow <jouanny.jerome@gmail.com>
2016-05-15 18:36:22 +0200

    test bug

--- app/src/main/java/com/example/inglow/mycoachsuprem/MyCoachSuprem.java
+++ app/src/main/java/com/example/inglow/mycoachsuprem/MyCoachSuprem.java
@@ -12,7 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-///Ss
+///S
 
 public class MyCoachSuprem extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
